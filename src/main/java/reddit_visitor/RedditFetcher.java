1716c9d6a6e3702dba5f1b3894756165cfff5349
Bruno Guerra <g.g.bruno.00@gmail.com>
2022-10-02 12:37:54 -0300

    Unused import

--- src/main/java/reddit_visitor/RedditFetcher.java
+++ src/main/java/reddit_visitor/RedditFetcher.java
@@ -14,7 +14,6 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
 public class RedditFetcher {
